5b2e416c66d0766ffa0b373b27e78f939a566e59
Karan Singh <urskaransingh4@gmail.com>
2025-07-04 16:19:30 +0530

    updated main file

--- Teamgrid/src/App.jsx
+++ Teamgrid/src/App.jsx
@@ -4,6 +4,9 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./components/theme";
 import Homepage from './pages/Homepage';
+import Navbar from './components/Navbar';
+import FooterSection from './components/FooterSection';
+import Frontend from './pages/Frontend';
 
 function App() {
 
@@ -11,7 +14,10 @@ function App() {
     <>
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Navbar/>
       <Homepage/>
+      {/* <Frontend/> */}
+      <FooterSection/>
       </ThemeProvider>
     </>
   )
