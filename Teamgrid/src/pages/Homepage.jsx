962cb31a16129db9d20345cb9f726d04d06baf93
Karan Singh <urskaransingh4@gmail.com>
2025-07-04 16:17:04 +0530

    updated homepage

--- Teamgrid/src/pages/Homepage.jsx
+++ Teamgrid/src/pages/Homepage.jsx
@@ -6,21 +6,29 @@ import WhyTeamgrid from '../components/WhyTeamgrid'
 import CapabilitiesSection from '../components/CapabilitiesSection'
 import TestimonialsSection from '../components/TestimonialsSection'
 import LetsTalk from '../components/Letstalk'
-import FooterSection from '../components/FooterSection'
-import Navbar from '../components/Navbar'
+import bgImage from '../assets/rectangle-25.png'
 
 function Homepage() {
+
+  const data = {
+    title: `Let’s Build Together`,
+    paraone:'— Efficiently, Flexibly, and Reliably',
+    paratwo:`Whether you're scaling a team or shipping a project, Teamgrid is your partner in dependable delivery.`,
+    btnone:`Let’s Talk`,
+    btntwo:'Request a Free Quote'
+  }
+
   return (
     <>
-    <Navbar/>
+    {/* <Navbar/> */}
     <Animated/>
     <WhoWeHelp/>
     <WhatWeDo/>
     <WhyTeamgrid/>
     <CapabilitiesSection/>
     <TestimonialsSection/>
-    <LetsTalk/>
-    <FooterSection/>
+    <LetsTalk bgImage={bgImage} title={data.title} paraone={data.paraone} paratwo={data.paratwo} btnone={data.btnone} btntwo={data.btntwo}/>
+    {/* <FooterSection/> */}
     </>
   )
 }
